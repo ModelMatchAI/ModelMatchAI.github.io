1c1e43aea65f561b5ea92e97db499778a4aefcfd
LukeHartzell1 <123661552+LukeHartzell1@users.noreply.github.com>
2025-03-29 13:23:21 -0400

    init commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // In Next 13, you can also set `output: "export"` here
+    output: "export",
+  };
+  
+  export default nextConfig;
+  
